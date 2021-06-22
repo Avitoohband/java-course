a45fb6ee3ef3b5fbe5c2b9ac2dc496368ac6d41f
Avi Tuchband <76391487+Avitoohband@users.noreply.github.com>
2021-06-22 20:40:39 +0300

    Add files via upload

---
+++ DictionaryProgram/src/dictionary/Menu.java
@@ -0,0 +1,14 @@
+package dictionary;
+
+public class Menu {
+
+    public static void showMenu() {
+        System.out.println("======Menu======\n" +
+                "1) Print Dictionary\n" +
+                "2) Add\n" +
+                "3) Edit\n" +
+                "4) Remove\n" +
+                "5) Show Menu\n" +
+                "Anything else to quit!\n");
+    }
+}
